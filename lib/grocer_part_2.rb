057e44d02a03dfe4ed4c9b9f299d226db36d66ca
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-15 20:06:09 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -77,7 +77,7 @@ def apply_clearance(cart)
   while i < cart.length
   
     if cart[i][:clearance] == true
-      cart[i][:price] -= cart[i][:price] / 5.0
+      cart[i][:price] -= cart[i][:price] / 5.0.round(2)
     end
     i += 1
   end
