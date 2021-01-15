10fdb6385396493e704f2da1b274c289d2630905
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-15 20:14:28 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -94,4 +94,8 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  
+  consolidate_cart(cart)
+  apply_coupons(cart, coupons)
+  apply_clearance(cart)
 end
